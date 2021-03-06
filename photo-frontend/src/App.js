7ca04ad55491448a7141b07a9fc5a4f73923a5b5
Brandon Nichols <brandonaronnichols@gmail.com>
2021-03-05 23:17:30 -0600

    successfully created frontend application that accepts images to upload to backend

--- photo-frontend/src/App.js
+++ photo-frontend/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ImageUploader from "react-images-upload";
+import axios from "axios";
 
 function App() {
+  const uploadURL = "put url here, not added to github to hide url";
+
+  const [currentImage, setCurrentImage] = useState(null);
+  const [imageData, setImageData] = useState({});
+
+  const selectImage = (failedImages, successImages) => {
+    // console.log("successImages: ", successImages);
+    const data = successImages[0];
+    const parts = data.split(";");
+
+    setImageData({
+      mime: parts[0].split(":")[1],
+      name: parts[1].split("=")[1],
+      data: parts[2]
+    });
+
+    setCurrentImage(successImages);
+  };
+
+  const submitImage = async (e) => {
+    e.preventDefault();
+    console.log("image upload data: ", imageData);
+    await axios.post(uploadURL, {
+      mime: imageData.mime,
+      name: imageData.name,
+      image: imageData.data
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageUploader
+        key="upload-from"
+        withIcon={true}
+        singleImage={true}
+        withPreview={true}
+        label="Maximum size file: 5MB"
+        buttonText="Select Image"
+        imgExtension={[".jpg", ".png", ".jpeg"]}
+        maxFileSize={5242880}
+        onChange={selectImage}
+      ></ImageUploader>
+      <button type="button" onClick={submitImage}>
+        Upload Image
+      </button>
+      <img src={currentImage} alt="sample of what it will look like" />
     </div>
   );
 }
