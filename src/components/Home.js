e145dabfa9128daf6f3c575b36e471451655a775
Brandon Nichols <brandonaronnichols@gmail.com>
2022-01-27 18:31:23 -0600

    added a list of clickable photograph names top right of page so users could jump to photos on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import styled, { keyframes, css } from "styled-components";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { fetchBucket } from "../actions";
 
 const HomeWrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: 0 18rem;
-`;
-
-const CenteredDiv = styled.div`
-  min-width: 100px;
-  min-height: 100px;
-  align-self: flex-start;
-  &:nth-child(even) {
-    align-self: flex-end;
-  }
-  margin: 500px 0;
 `;
 
 const fadeIn = keyframes`
@@ -29,10 +20,44 @@ const fadeIn = keyframes`
   }
 `;
 
+const CenteredDiv = styled.div`
+  display: flex;
+  min-width: 100px;
+  min-height: 100px;
+  &:nth-child(even) {
+    flex-direction: row-reverse;
+  }
+  margin: 500px 0;
+`;
+
+const Title = styled.h2`
+  animation: ${fadeIn} 2s;
+  padding: 100px;
+`;
+
 const Image = styled.img`
+  padding: 100px;
   max-width: 500px;
   max-height: 500px;
   animation: ${fadeIn} 2s;
+`;
+
+const QuickScrollContainer = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  top: 0;
+  right: 0;
+  height: min-content;
+  background-color: black;
+  opacity: 0.5;
+
+  a {
+    text-decoration: none;
+    color: white;
+    margin: 10px 0;
+    margin-left: 5px;
+  }
 `;
 
 const Home = (props) => {
@@ -48,11 +73,12 @@ const Home = (props) => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.children[0].style.display = "flex";
+          entry.target.children[0].style.display = "inline";
+          entry.target.children[1].style.display = "inline";
         } else {
           entry.target.children[0].style.display = "none";
+          entry.target.children[1].style.display = "none";
         }
-        // console.log(entry);
       });
     });
 
@@ -72,11 +98,22 @@ const Home = (props) => {
           <CenteredDiv
             key={index}
             ref={(el) => (homeSection.current[index] = el)}
+            id={`${imgObj["photo-name"]}`}
           >
             <Image src={imgObj.link} alt="" />
+            <Title>{imgObj["photo-name"]}</Title>
           </CenteredDiv>
         );
       })}
+      <QuickScrollContainer>
+        {props.imageContainers.map((imgObj, index) => {
+          return (
+            <a key={index} href={`#${imgObj["photo-name"]}`}>
+              {imgObj["photo-name"]}
+            </a>
+          );
+        })}
+      </QuickScrollContainer>
     </HomeWrapper>
   );
 };
